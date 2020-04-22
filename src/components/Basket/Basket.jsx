d94d150363efe951c6db736481fb0537572b746b
Maxim Rudenko <m.rudenko@cis.sumdu.edu.ua>
2020-04-22 18:07:46 +0300

    feat(basket) add button checkout

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -2,63 +2,78 @@ import React from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
+import {DialogActions} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 import BasketProduct from "./BasketProduct/BasketProduct";
+import useStyles from "./styles";
 
 function Basket({open, onClose, basket, handleDeleteProduct, setProductCount}) {
+    const classes = useStyles();
+
     const baskIsEmpty = !basket.length;
     const total = basket.reduce((total, product) => total + (product.count * product.price), 0);
 
     return (
         <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-            <DialogTitle>
+            <DialogTitle className={classes.dialogTitle}>
                 Basket
             </DialogTitle>
             <DialogContent>
                 {baskIsEmpty && (
-                    <Grid container alignItems="center">
-                        <Grid item xs={12}>
-                            <Grid container justify="center">
-                                <Typography variant="h5">
-                                    Empty basket....
-                                </Typography>
-                            </Grid>
-                        </Grid>
+                    <Grid container justify="center">
+                        <Typography variant="body1">
+                            Empty basket....
+                        </Typography>
                     </Grid>
                 )}
                 {!baskIsEmpty && (
                     <Grid container spacing={2}>
                         <Grid item xs={4}>
-                            <Typography variant="h6" >Title</Typography>
+                            <Typography variant="body1">Title</Typography>
                         </Grid>
                         <Grid item xs={3}>
-                            <Typography variant="h6">Price</Typography>
+                            <Typography variant="body1">Price</Typography>
                         </Grid>
                         <Grid item xs={3}>
-                            <Typography variant="h6">Count</Typography>
+                            <Typography variant="body1">Count</Typography>
                         </Grid>
                         <Grid item xs={2}/>
                         {basket.map(product => <BasketProduct product={product} handleDeleteProduct={handleDeleteProduct} setProductCount={setProductCount}/>)}
                     </Grid>
                 )}
             </DialogContent>
-            <Grid container>
-                <Grid item xs={4}>
-                    Back
-                </Grid>
-                <Grid item xs={4}>
-                    Buy
-                </Grid>
-                <Grid item xs={4}>
-                    <Grid container justify="center">
-                        <Typography variant="h5">
-                            {`Total: $${total}`}
-                        </Typography>
+            <DialogActions className={classes.dialogActions}>
+                <Grid container>
+                    <Grid item container xs={4} spacing={1}>
+                        <Grid item xs={6}>
+                            <Button variant="contained" onClick={onClose}>
+                                Cancel
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                to={'/checkout' }
+                            >
+                                Checkout
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={3}>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Grid container justify="center">
+                            <Typography variant="h5">
+                                {`Total: $${total}`}
+                            </Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            </DialogActions>
         </Dialog>
     );
 }
